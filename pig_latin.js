680f851c93201876c7b247f6f618097126e03f5c
Adith <adithwidyapradipta@gmail.com>
2017-06-20 21:35:23 +0700

    pig latin done

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,49 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'PIGLATIN $ '
+})
+
+rl.prompt();
+
+rl.on('close', function() {
+  console.log('\n------bye');
+})
+rl.on('line', (data) => {
+  console.log('Converted: ' + pigLatin(data));
+  // if (data == "hello")
+  //   rl.setPrompt('Case hello > ')
+  // else {
+  //   rl.setPrompt('Try again > ')
+  // }
+  // rl.close()
+  rl.prompt();
+});
+
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  var consonant = /(\b[^aiueo]+)(\w+)/;
+  var sentenceArr = sentence.split(' ') //['hello', 'hello', 'ibu', 'wkwk']
+  var pigLatin = [];
+
+  for (let i = 0; i < sentenceArr.length; i++) {
+    if (consonant.test(sentenceArr[i]) === true) {
+      var converted = sentenceArr[i].replace(consonant, '$2$1ay');
+      pigLatin.push(converted);
+    } else {
+      pigLatin.push(sentenceArr[i])
+    }
+  }
+  return pigLatin.join(' ');
 }
+
+
+
+
+
+
+
+// console.log(pigLatin('hello hello ibu hello wkwk'));
