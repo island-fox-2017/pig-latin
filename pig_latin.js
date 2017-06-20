84e652d7ba5acfe3fd8a1fd4b8df916a595098e9
pdvega <pdvegaa@gmail.com>
2017-06-20 15:54:45 +0700

    piglatin done

--- pig_latin.js
+++ pig_latin.js
@@ -6,15 +6,37 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output:process.stdout,
-  prompt: 'Input your words >>> '
+  prompt: 'Input your sentence >>> '
 })
 
 rl.prompt()
+rl.on('line', (sentence) => {
+  console.log(pigLatin(sentence));
+})
+
+rl.on('close', () => {
+  console.log('Sayonara!');
+})
 
 function pigLatin(sentence) {
   // Your pig latin implementation here...
   
-  let regex = /\b[aiueo]/
+  let regVow = /\b[aiueo]/ //define vowels sentence 
+  let regCons = /\b[^aiueo\s]/g, // \b untuk mengecek per kata
+    result = [],
+    i,
+    replace,
+    match;
   
-  
+  let word = sentence.split(' ')
+  for(i=0; i<word.length; i++){
+    if(regCons.test(word[i])){
+      replace = word[i].replace(regCons, '')
+      match = word[i].match(regCons)
+      result.push(replace + match + 'ay')
+    }else if(regVow.test(word[i])){
+      result.push(word[i])
+    }
+  }
+  return result.join(' ')
 }
