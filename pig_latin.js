e4ea0e4fe4ee7349cf4e64356e5257470d20e736
Tamastro <rpratama45@gmail.com>
2017-06-20 19:05:52 +0700

    pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -1,8 +1,32 @@
 "use strict"
 
-//use readline to fix this challenge
 const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Input : '
+});
+rl.prompt();
+rl.on('line', (pig) => {
+  pig = pigLatin(pig);
+  console.log(`Received: ${pig}`);
+  rl.prompt();
+});
+
+
 
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  var spl = sentence.split(' ');
+  var isi=[];
+  for (let i = 0; i < spl.length; i++) {
+    if (/^[aeiou]/i.test(spl[i])) {
+      isi.push(spl[i]);
+    }
+    else {
+      isi.push(spl[i].replace(/^([^aeiou]+)(.*)/i, "$2$1ay"));
+    }
+  }
+  return isi.join(' ')
 }
