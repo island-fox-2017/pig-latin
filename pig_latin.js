df4f44130a4f2dc1fc449cc56abee63c7df62b6e
biliman <fickry.bil.iman@gmail.com>
2017-06-20 16:27:24 +0700

    pig latin done

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,38 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt('===== PIGLATIN =====\nType Your Words Here: ')
+rl.prompt();
+
+rl.on('line', (input)=> {
+  console.log('Pig Latin : ', pigLatin(input));
+  rl.prompt();
+}).on('close', ()=> {
+  console.log('\n=== Have a nice days ===');
+})
+
+
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  let arrSentence = sentence.split(' ');
+  let regex = /[aeiou]/i;
+  let arr = [];
+  
+  arrSentence.forEach(function (element) {
+    let index = element.search(regex); // output is index number of string..
+    
+    if (index === 0) {
+      arr.push(element);
+    } else {
+      arr.push(element.slice(index) + element.slice(0,1) + 'ay')
+    }
+  });
+  return arr.join(' ');
+  
 }
+
