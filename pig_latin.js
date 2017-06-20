6fcfc2695d90e6bb7eb4b8ff5dff38797f9d9c8b
budhip <ryo.bunraku@yahoo.com>
2017-06-20 21:19:34 +0700

    Commit tugas pig-latin.js

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,27 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Masukan kalimat: '
+});
+rl.prompt();
+rl.on('line', (kalimat) => {
+  kalimat = pigLatin(kalimat);
+  console.log(`Output: ${kalimat}`);
+  rl.prompt();
+});
+
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
+  var perkata = sentence.split(' ');
+  var newSentence = [];
+  for (var i = 0; i < perkata.length; i++){
+    if (perkata[i].charAt(0).match(/[aiueo]/gi)) {
+      newSentence.push(perkata[i]);
+    } else {
+      newSentence.push(perkata[i].replace(/(^[^aiueo]*)(.*)/i, '$2$1ay'))
+    }
+  }
+  return newSentence.join().replace(/,/g, ' ');
 }
