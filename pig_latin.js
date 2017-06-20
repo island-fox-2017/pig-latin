d6d34224932bb6588ae656d7677e2babc47dcc2a
pdvega <pdvegaa@gmail.com>
2017-06-20 15:17:46 +0700

    piglatin readline

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,18 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output:process.stdout,
+  prompt: 'Input your words >>> '
+})
+
+rl.prompt()
+
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  
+  let regex = /\b[aiueo]/
+  
+  
 }
