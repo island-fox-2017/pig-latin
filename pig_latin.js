c333a0f10db40ef2d5122c396dd5ccc33b0f9efe
pdvega <pdvegaa@gmail.com>
2017-06-22 14:29:21 +0700

    rev regex cons

--- pig_latin.js
+++ pig_latin.js
@@ -12,6 +12,7 @@ const rl = readline.createInterface({
 rl.prompt()
 rl.on('line', (sentence) => {
   console.log(pigLatin(sentence));
+  rl.prompt();
 })
 
 rl.on('close', () => {
@@ -22,17 +23,15 @@ function pigLatin(sentence) {
   // Your pig latin implementation here...
   
   let regVow = /\b[aiueo]/ //define vowels sentence 
-  let regCons = /\b[^aiueo\s]/g, // \b untuk mengecek per kata
-    result = [],
-    i,
-    replace,
-    match;
+  // let regCons = /\b[^aiueo\s]/g // \b untuk mengecek per kata
+  let regCons = /^[bcdfghjklmnpqrstvwxyz]+/i
+  let result = [];
   
   let word = sentence.split(' ')
-  for(i=0; i<word.length; i++){
+  for(let i=0; i<word.length; i++){
     if(regCons.test(word[i])){
-      replace = word[i].replace(regCons, '')
-      match = word[i].match(regCons)
+      let replace = word[i].replace(regCons, '')
+      let match = word[i].match(regCons)
       result.push(replace + match + 'ay')
     }else if(regVow.test(word[i])){
       result.push(word[i])
