5cb4be54c3a291c6e76b9f2da31266bda7479dc7
achimbaggins <achim_baggins@yahoo.com>
2017-07-01 09:53:10 +0700

    tugas pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,7 +2,36 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Masukkan sembarang kata atau Phrase : '
+});
 
+rl.prompt();
+
+rl.on('line', (input) => {
+  //hasil dari pengerjaan function ditulis ke console juga
+  console.log(`####### Kata yang dihasilkan :  '${pigLatin(input)}'`);
+  rl.prompt();
+})
+.on('close', () => {
+  console.log('Sampai jumpa lagi ya!');
+  process.exit(0);
+});
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  let splitSentence = sentence.split(' ');
+  let output= [];
+
+  for(let i=0; i<splitSentence.length; i++) {
+      if(/^[aiueo]/.test(splitSentence[i])) {
+       output.push(splitSentence[i]);
+      } else {
+       output.push(splitSentence[i].replace(/(^[^aiueo]+)(.*)/, '$2$1ay'));
+      }
+  }
+
+  return output.join(' ');
+
 }
